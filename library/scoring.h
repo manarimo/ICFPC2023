5bff7717412ed238d648ed05eeac82fcae399439
Shunsuke Ohashi <amylase.inquiry@gmail.com>
2023-07-08 05:33:09 +0900

    re

--- library/scoring.h
+++ library/scoring.h
@@ -52,74 +52,85 @@ namespace manarimo {
         }
         return angle;
     }
+
+    vector<int> get_unblocked_attendees_of_musician(const problem_t& problem, const vector<P>& placements, const int musician_id) {
+        const int n_musician = problem.musicians.size();
+        const int n_attendee = problem.attendees.size();
+
+        vector<int> unblocked_attendees;
+        const cP center = {placements[musician_id].first, placements[musician_id].second};
+
+        struct event {
+            int type;  // 0: block start, 1: attendee, 2: block end
+            int index;  // musician id or attendee id
+        };
+        vector<event> event_infos;
+        vector<pair<number, int>> events;
+        // add event type=1
+        for (int i_attendee = 0; i_attendee < n_attendee; i_attendee++) {
+            const auto& attendee = problem.attendees[i_attendee];
+            const cP attendee_position = {attendee.x, attendee.y};
+            const number argument = arg(attendee_position - center);
+            events.emplace_back(argument, event_infos.size());
+            event_infos.push_back({1, i_attendee});
+        }
+
+        // add event type=0, 2
+        // assuming that musician distance is at least 10 (> 5)
+        int overlapping_spans = 0;
+        for (int j_musician = 0; j_musician < n_musician; j_musician++) {
+            if (j_musician == musician_id) {
+                continue;
+            }
+            const number block_distance = 5;
+            const cP location = {placements[j_musician].first, placements[j_musician].second};
+            const cP vec = location - center;
+            const number distance = abs(vec);
+            const number vec_argument = arg(vec);
+            const number offset = asin(block_distance / distance);
+
+            const number start = normalize_angle(vec_argument - offset);
+            const number end = normalize_angle(vec_argument + offset);
+            if (start > end) {
+                overlapping_spans += 1;
+            }
+            events.emplace_back(start, event_infos.size());
+            event_infos.push_back({0, j_musician});
+            events.emplace_back(end, event_infos.size());
+            event_infos.push_back({2, j_musician});
+        }
+
+        sort(events.begin(), events.end());
+        for (const auto event: events) {
+            const auto event_info = event_infos[event.second];
+            switch (event_info.type) {
+                case 0:
+                    overlapping_spans += 1;
+                    break;
+                case 1:
+                    if (overlapping_spans == 0) {
+                        unblocked_attendees.emplace_back(musician_id, event_info.index);
+                    }
+                    break;
+                case 2:
+                    overlapping_spans -= 1;
+                    break;
+                default:
+                    throw -1;
+            } 
+        }
+        return unblocked_attendees;
+    }
     
     vector<pair<int, int>> get_unblocked_pairs(const problem_t& problem, const vector<P>& placements) {
         const int n_musician = problem.musicians.size();
-        const int n_attendee = problem.attendees.size();
         
         vector<pair<int, int>> unblocked_pairs;
-
         for (int i_musician = 0; i_musician < n_musician; i_musician++) {
-            const cP center = {placements[i_musician].first, placements[i_musician].second};
-
-            struct event {
-                int type;  // 0: block start, 1: attendee, 2: block end
-                int index;  // musician id or attendee id
-            };
-            vector<event> event_infos;
-            vector<pair<number, int>> events;
-            // add event type=1
-            for (int i_attendee = 0; i_attendee < n_attendee; i_attendee++) {
-                const auto& attendee = problem.attendees[i_attendee];
-                const cP attendee_position = {attendee.x, attendee.y};
-                const number argument = arg(attendee_position - center);
-                events.emplace_back(argument, event_infos.size());
-                event_infos.push_back({1, i_attendee});
-            }
-
-            // add event type=0, 2
-            // assuming that musician distance is at least 10 (> 5)
-            int overlapping_spans = 0;
-            for (int j_musician = 0; j_musician < n_musician; j_musician++) {
-                if (j_musician == i_musician) {
-                    continue;
-                }
-                const number block_distance = 5;
-                const cP location = {placements[j_musician].first, placements[j_musician].second};
-                const cP vec = location - center;
-                const number distance = abs(vec);
-                const number vec_argument = arg(vec);
-                const number offset = asin(block_distance / distance);
-
-                const number start = normalize_angle(vec_argument - offset);
-                const number end = normalize_angle(vec_argument + offset);
-                if (start > end) {
-                    overlapping_spans += 1;
-                }
-                events.emplace_back(start, event_infos.size());
-                event_infos.push_back({0, j_musician});
-                events.emplace_back(end, event_infos.size());
-                event_infos.push_back({2, j_musician});
-            }
-
-            sort(events.begin(), events.end());
-            for (const auto event: events) {
-                const auto event_info = event_infos[event.second];
-                switch (event_info.type) {
-                    case 0:
-                        overlapping_spans += 1;
-                        break;
-                    case 1:
-                        if (overlapping_spans == 0) {
-                            unblocked_pairs.emplace_back(i_musician, event_info.index);
-                        }
-                        break;
-                    case 2:
-                        overlapping_spans -= 1;
-                        break;
-                    default:
-                        throw -1;
-                } 
+            for (auto i_attendee: get_unblocked_attendees_of_musician(
+                problem, placements, i_musician
+            )) {
+                unblocked_pairs.emplace_back(i_musician, i_attendee);
             }
         }
 
