419c35069d1ce8803a0e48c5a0f068e1f30e7e3a
Osamu Koga <osak.63@gmail.com>
2023-07-07 21:42:25 +0900

    problems

---
+++ download.ts
@@ -0,0 +1,28 @@
+const API = "https://api.icfpcontest.com/problem";
+
+async function download(id: number, path: string) {
+    console.log(`Downloading problem ${id}...`);
+    const response = await fetch(`${API}?problem_id=${id}`, {
+        headers: {
+            'Authorization': `Bearer ${Deno.env.get('ICFPC_TOKEN')}`
+        }
+    });
+    if (response.status != 200) {
+        throw new Error(`Failed to fetch response: problem id=${id}`);
+    }
+
+    const body = await response.json();
+    if ('Success' in body) {
+        await Deno.writeTextFile(`${path}/${id}.json`, body['Success']);
+    } else {
+        throw new Error(`API returned failure response: ${body['Failure']}`);
+    }
+}
+
+async function main() {
+    for (let i = 1; i <= 45; i++) {
+        await download(i, 'problems');
+    }
+}
+
+await main();
