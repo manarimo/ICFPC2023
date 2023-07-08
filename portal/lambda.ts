660b4a1eb5340a838b37ed78d9414b04092dc4ec
kenkoooo <kenkou.n@gmail.com>
2023-07-08 18:28:41 +0900

    add apis

--- portal/lambda.ts
+++ portal/lambda.ts
@@ -1,41 +1,94 @@
-import { Context, APIGatewayProxyResultV2, APIGatewayProxyEventV2 } from 'aws-lambda';
-import { GatewayEvent } from "./types";
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
+  Context,
+} from "aws-lambda";
 import { submitSolutionHandler } from "./submit_solution";
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
-const dbHost = process.env['POSTGRES_HOST']!;
-const password = process.env['POSTGRES_PASSWORD']!;
+const dbHost = process.env["POSTGRES_HOST"]!;
+const password = process.env["POSTGRES_PASSWORD"]!;
 
 const pg = new Pool({
-    user: 'postgres',
-    database: 'icfpc2023',
-    host: dbHost,
-    port: 5432,
-    password: password
+  user: "postgres",
+  database: "icfpc2023",
+  host: dbHost,
+  port: 5432,
+  password: password,
 });
-pg.on('error', (err) => {
-  console.error('pg error', err);
+pg.on("error", (err) => {
+  console.error("pg error", err);
 });
 
 export async function handler(
-  event: APIGatewayProxyEventV2 & {path: string},
+  event: APIGatewayProxyEventV2 & { path: string },
   context: Context,
 ): Promise<APIGatewayProxyResultV2> {
   console.log(event);
-    let result: any;
-    if (event.path == '/solutions/submit') {
-        result = await submitSolutionHandler(event, context, pg);
-    } else {
-        const q = await pg.query('SELECT * FROM problems');
-        result = {
-            size: q.rowCount,
-            firstRow: q.rows[0]
-        };
-    }
+  let result: any;
+  if (event.path == "/solutions/submit") {
+    result = await submitSolutionHandler(event, context, pg);
+  } else {
+    result = await apiHandler(event);
+  }
 
   return {
     body: JSON.stringify(result),
-    headers: { "content-type": "text/html;charset=utf8" },
+    headers: { "content-type": "application/json;charset=utf8" },
     statusCode: 200,
   };
+}
+
+async function apiHandler(event: APIGatewayProxyEventV2 & { path: string }) {
+  switch (event.path) {
+    case "/problems": {
+      type ProblemRow = {
+        problem_id: number;
+        room_width: number;
+        room_height: number;
+        num_musicians: number;
+        num_attendees: number;
+      };
+      const result = await pg.query<ProblemRow>("SELECT * FROM problems");
+      const problems = result.rows.map((
+        { problem_id, room_height, room_width, num_attendees, num_musicians },
+      ) => ({
+        problemId: problem_id,
+        roomHeight: room_height,
+        roomWidth: room_width,
+        numAttendees: num_attendees,
+        numMusicians: num_musicians,
+      }));
+      return {
+        problems,
+      };
+    }
+    case "/solutions": {
+      type SolutionRow = {
+        id: number;
+        solver_name: string;
+        problem_id: number;
+        score: number;
+        solution_path: string;
+      };
+      const result = await pg.query<SolutionRow>("SELECT * FROM solutions");
+      const solutions = result.rows.map((
+        { id, solver_name, problem_id, score, solution_path },
+      ) => ({
+        id,
+        solverName: solver_name,
+        problemId: problem_id,
+        score,
+        solutionPath: solution_path,
+      }));
+      return {
+        solutions,
+      };
+    }
+    default: {
+      return {
+        message: "hello manarimo",
+      };
+    }
+  }
 }
