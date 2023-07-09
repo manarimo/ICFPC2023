563eb0314b004b3ddb2228b92173774f447a13aa
Osamu Koga <osak.63@gmail.com>
2023-07-10 01:01:53 +0900

    Issue SQL in looper

--- portal/looper/index.ts
+++ portal/looper/index.ts
@@ -1,5 +1,7 @@
 import { Context } from "aws-lambda";
-import { SolverRunnerEvent } from "../solver_runner";
+import { SolverRunnerEvent, SolverRunnerResponse } from "../solver_runner";
+import { Pool } from "pg";
+import { discordSay } from "../util/discord";
 
 interface LooperEvent {
     label: string;
@@ -9,6 +11,7 @@ interface LooperEvent {
     limit?: number;
     env?: Record<string, string>;
     exclude?: number[];
+    response?: (SolverRunnerResponse | null)[];
 }
 
 type LooperResponseToMap = {
@@ -21,33 +24,79 @@ interface LooperResponseToEnd {
 
 type LooperResponse = LooperResponseToMap | LooperResponseToEnd;
 
-const discordWebhook = process.env['DISCORD_WEBHOOK']!;
+const dbHost = process.env['POSTGRES_HOST']!;
+const password = process.env['POSTGRES_PASSWORD']!;
 
-async function say(message: string): Promise<void> {
-    await fetch(discordWebhook, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            content: message
-        })
-    });
+const pg = new Pool({
+    user: 'postgres',
+    database: 'icfpc2023',
+    host: dbHost,
+    port: 5432,
+    password: password
+});
+pg.on('error', (err) => {
+  console.error('pg error', err);
+});
+
+async function determineBestSeeds(): Promise<Record<number, string>> {
+    const response = await pg.query(`
+        with g as (
+            select *,
+            rank() over (partition by problem_id order by score desc, id asc) as r
+            from solutions
+        )
+        select *
+        from g
+        where r=1; 
+    `);
+
+    const result: Record<number, string> = {};
+    for (const row of response.rows) {
+        result[row['problem_id']] = row['solver_name'];
+    }
+    return result;
+}
+
+async function saveResponse(responses: (SolverRunnerResponse | null)[]): Promise<void> {
+    const placeholders: string[] = [];
+    const values: any[] = [];
+    const responsesNonNull = responses.filter((r) => r != null) as SolverRunnerResponse[];
+    responsesNonNull.forEach((r, i) => {
+        placeholders.push(`($${i*4 + 1}, $${i*4 + 2}, $${i*4 + 3}, $${i*4 + 4})`);
+        values.push(r.solverName);
+        values.push(r.problemId);
+        values.push(r.score);
+        values.push(`solutions/${r.solverName}/${r.problemId}.json`);
+    })
+
+    const res = await pg.query(
+        'INSERT INTO solutions (solver_name, problem_id, score, solution_path)'
+        + ' VALUES '
+        + placeholders.join(',')
+        + ' ON CONFLICT ON CONSTRAINT solutions_solver_name_problem_id_key'
+        + ' DO UPDATE SET score=excluded.score, solution_path=excluded.solution_path ', values);
+    console.log(res);
 }
 
 export async function handler(
   event: LooperEvent,
   context: Context,
 ): Promise<LooperResponse> {
+    if (event.response) {
+        await saveResponse(event.response);
+    }
+
     if (event.limit && event.count > event.limit) {
-        await say(`${event.label}が終わったんじゃも！`);
+        await discordSay(`${event.label}が終わったんじゃも！`);
         return {
             end: true
         };
     }
 
     // Make Kolog work
-    await say(`${event.label}のループ${event.count}回目をじっこうします！`);
+    await discordSay(`${event.label}のループ${event.count}回目をじっこうします！`);
+
+    const bestSeeds = await determineBestSeeds();
 
     const items: SolverRunnerEvent[] = [];
     for (let i = 1; i <= 90; ++i) {
@@ -68,13 +117,16 @@ export async function handler(
         }
 
         if (solverPath) {
-            items.push({
+            const item: SolverRunnerEvent = {
                 problemId: i,
                 solverPath: solverPath,
                 solverName: `${event.label}-${event.count}`,
-                seed: '__best__',
                 env: event.env
-            });
+            };
+            if (bestSeeds[i]) {
+                item.seed = bestSeeds[i];
+            }
+            items.push(item);
         }
     }
 
