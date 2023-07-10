33010e03c7d9a6ead8df50c209c6106655a64cd3
Osamu Koga <osak.63@gmail.com>
2023-07-10 20:43:15 +0900

    boring

--- osak/seeds.rb
+++ osak/seeds.rb
@@ -3,12 +3,14 @@
 require 'set'
 require 'json'
 
-include_list = Set.new([8, 2, 1, 12, 30])
+#include_list = Set.new([29, 6, 12, 30, 22, 23, 24, 25, 26, 27, 28])
+include_list = Set.new([1,2,8])
 exclude_list = (1..90).select {|n| !include_list.include?(n)}
+base_name = "boring"
 
 (1..20).each do |i|
   payload = JSON.dump({
-    "label": "top-seed-#{i}",
+    "label": "#{base_name}-#{i}",
     "count": 1,
     "limit": 20,
     "lightningSolver": "kawatea_block",
@@ -18,10 +20,10 @@ exclude_list = (1..90).select {|n| !include_list.include?(n)}
       "MAX_DISTANCE_POW_MIN": 0,
       "MAX_DISTANCE_POW_MAX": 3.5
     },
-    "tag": "top-seed-#{i}"
+    "tag": "#{base_name}-#{i}"
   })
 
-  cmdline = "aws stepfunctions start-execution --state-machine-arn arn:aws:states:ap-northeast-1:145924489351:stateMachine:ICFPC2023-step-functions --name top-seed-#{i} --input '#{payload}'"
+  cmdline = "aws stepfunctions start-execution --state-machine-arn arn:aws:states:ap-northeast-1:145924489351:stateMachine:ICFPC2023-step-functions --name #{base_name}-#{i} --input '#{payload}'"
   puts cmdline
   `#{cmdline}`
 end
